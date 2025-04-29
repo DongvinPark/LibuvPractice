9ea57e06f2e8815b087f6eb20cb7c2760de01e43
dongvin99 <dongvin99@naver.com>
2025-04-29 14:57:58 +0900

    added glib hash map example

--- src/main.c
+++ src/main.c
@@ -2,18 +2,27 @@
 // Created by dongvin on 25. 4. 27.
 //
 #include <stdio.h>
-#include <stdlib.h>
-#include <uv.h>
+#include <glib.h>
 
+void print_key_value(gpointer key, gpointer value, gpointer user_data)
+{
+    (void)user_data;
+    printf("%s => %s\n", (char*)key, (char*)value);
+}
 
 int main() {
-    uv_loop_t *loop = malloc(sizeof(uv_loop_t));
-    uv_loop_init(loop);
+    GHashTable* table = g_hash_table_new(g_str_hash, g_str_equal);
 
-    printf("Now quitting.\n");
-    uv_run(loop, UV_RUN_DEFAULT);
+    g_hash_table_insert(table, "name", "Alice");
+    g_hash_table_insert(table, "city", "Wonderland");
+    g_hash_table_insert(table, "job", "Adventurer");
 
-    uv_loop_close(loop);
-    free(loop);
+    const char* name = g_hash_table_lookup(table, "name");
+    printf("\nAll k-v pairs:\n");
+    g_hash_table_foreach(
+        table, print_key_value, NULL
+    );
+
+    g_hash_table_destroy(table);
     return 0;
 }
