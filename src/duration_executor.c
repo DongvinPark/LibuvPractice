081aaa61551f4cedae72c9c8c35f340d229608af
DongvinPark <dongvin99@naver.com>
2025-05-01 06:04:14 +0900

    updated uv closing logic

--- src/duration_executor.c
+++ src/duration_executor.c
@@ -24,16 +24,32 @@ static void on_idle(uv_idle_t* handle)
     }
 }
 
+// 이러한 종료 핸들러를 이용해서 자원을 할당 해제하는 것이 안전하다.
+// 종료 핸들러 없이 on_timer() 내부의 마지막 부분에서 그냥 바로 free(context);
+// 해버리면 윈도우 OS의 libuv에서는 context를 아직 사용 중인데 갑자기 할당 해제를 해버리면서
+// Assertion failed: 0, file C:\vcpkg\buildtrees\libuv\src\v1.49.2-5cffa54d5b.clean\src\win\handle-inl.h, line 160
+// 라는 에러 메시지와 함께 프로그램이 종료돼 버린다.
+static void on_close(uv_handle_t* handler) {
+    duration_executor_context_t* context = (duration_executor_context_t*)handler->data;
+    free(context);
+}
+
 static void on_timer(uv_timer_t* handle)
 {
     duration_executor_context_t* context = (duration_executor_context_t*)handle->data;
     printf("Duration expired - stopping task...\n");
 
     uv_idle_stop( &(context->idle) );
-    uv_close( (uv_handle_t*)&(context->idle), NULL);
-    uv_close( (uv_handle_t*)&(context->timer), NULL);
 
-    free(context);
+    if (!uv_is_closing( (uv_handle_t*)&(context->idle) ))
+    {
+        uv_close( (uv_handle_t*)&(context->idle), NULL);
+    }
+
+    if (!uv_is_closing( (uv_handle_t*)&(context->timer) ))
+    {
+        uv_close( (uv_handle_t*)&(context->timer), on_close);
+    }
 }
 
 void async_nonblocking_duration_executor(
