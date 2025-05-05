aecde5987632c10ddb5b8c38df7119c024893633
dongvin99 <dongvin99@naver.com>
2025-05-05 18:23:53 +0900

    implemented async nonblocking file reading with uv_fs_read()

--- src/main.c
+++ src/main.c
@@ -2,163 +2,156 @@
 // Created by dongvin on 25. 4. 27.
 //
 #include <stdio.h>
+#include <stdlib.h>
 #include <uv.h>
-#include <glib.h>
-#include <unistd.h> // to use 'read()' function
-#include <errno.h> // to use 'errno'
+#include <unistd.h>
 
 #define BUFFER_SIZE 5
-
-// for ubuntu linux
-//#define FILE_PATH "/home/dongvin/Documents/LibuvPractice/src/example.txt"
-
-// for macOS
 #define FILE_PATH "/Users/dongvin99/Documents/LibuvPractice/src/example.txt"
 
-// for window
-
+// 파일 작업 컨텍스트 객체
 typedef struct
 {
-    uv_fs_t open_req;
-    uv_fs_t read_req;
-    uv_buf_t buffer;
     uv_loop_t* loop;
-    int fd;
-    ssize_t read_bytes;
+    uv_file file;
+    ssize_t offset;
+    uv_fs_t close_req;
 } file_context_t;
 
-// 'work callback' and 'after work callback' for libuv's thread pool
-// 할당된 버퍼를 이용해서 파일의 내용물을 읽어들이는 작업은 여기에서 진행된다.
-void work_cb(uv_work_t* req)
+// 파일 리딩 작업 요청 객체
+typedef struct
 {
-    file_context_t* ctx = (file_context_t*)req->data;
-    ctx->read_bytes = read(ctx->fd, ctx->buffer.base, ctx->buffer.len);
+    uv_fs_t req;
+    char* buffer;
+    // 현재 파일 리딩 작업에서 참조해야 하는 '파일 작업 컨텍스트 객체'의 포인터를 저장해 놓는다.
+    file_context_t* ctx;
+} fs_req_wrap_t;
 
-    if (ctx->read_bytes < 0)
-    {
-        int err = errno;
-        fprintf(stderr, "Read error: %s\n", uv_strerror(err));
-    }
+
+void on_close(uv_fs_t* req)
+{
+    // libuv 내부에서 req 와 관련된 다른 자원들을 전부 해제할 수 있게 해준다.
+    // 단, req 자체를 할당 해제하는 것은 아니다.
+    uv_fs_req_cleanup(req);
+
+    file_context_t* ctx = req->data;
+    printf("\nFile closed\n");
+    // 파일 작업 컨텍스트 객체를 회수한다.
+    free(ctx);
 }
 
-// 파일 리딩 후, 후 처리 작업은 여기에서 진행된다.
-// 총 세 가지 경우의 수가 존재한다. Error / EOF / Continue Reading 이다.
-void after_work_cb(uv_work_t* req, int status)
-{
-    file_context_t *ctx = (file_context_t *)req->data;
-
-    if (ctx->read_bytes < 0)
-    {   // Error
-        fprintf(stderr, "Async read failed: %s\n", uv_strerror((int)ctx->read_req.result));
-        uv_fs_close(ctx->loop, &ctx->open_req, ctx->fd, NULL);
-        free(ctx->buffer.base);
-        free(ctx);
-        free(req);
-        printf("!!! buffer, ctx, req freed with error !!!\n");
-        return;
-    }
-    else if (ctx->read_bytes == 0)
-    {
-        // EOF. cleanup
-        uv_fs_close(ctx->loop, &ctx->open_req, ctx->fd, NULL);
-        free(ctx->buffer.base);
-        free(ctx);
-        free(req);
-        printf("!!! buffer, ctx, req freed with EOF !!!\n");
-        return;
-    }
-    else
-    {   // print elements in buffer and enqueue next async nonblocking reading task.
-
-        // Ensure buffer is null-terminated before using strtok
-        ctx->buffer.base[ctx->read_bytes] = '\0';
-
-        // Print lines from buffer
-        char *line = strtok(ctx->buffer.base, "\n");
-        while (line != NULL) {
-            printf("line: %s\n", line);
-            line = strtok(NULL, "\n");
-        }
-
-        // Queue the next read
-        printf("\t!!! new work req allocated !!!\n");
-        uv_work_t *new_req = malloc(sizeof(uv_work_t));
-        new_req->data = ctx;
-        uv_queue_work(ctx->loop, new_req, work_cb, after_work_cb);
-    }
-
-    // 위의 경우의 수와 상관없이, 현재 after_work_cb 호출의 파라미터로 전달된 req는 free 해야 한다.
-    uv_fs_req_cleanup(&ctx->read_req);
-    free(req); // Always free the current request
-    printf("\t!!! current req freed !!!\n");
-}
+// 먼저 선언 해둔다.
+void on_read(uv_fs_t* req);
 
 void on_open(uv_fs_t* req)
 {
-    printf("!!! on_open called !!!\n");
-    if (req->result < 0)
+    file_context_t* ctx = req->data;
+
+    if (req->result >= 0)
     {
-        fprintf(stderr, "Error opening file: %s\n", uv_strerror((int)req->result));
-        return;
+        ctx->file = (uv_file)req->result;
+
+        // 최초의 파일 리딩 작업 요청 객체를 할당하고, 히딩 작업에 필요한 버퍼도 할당한다.
+        fs_req_wrap_t* wrap = malloc(sizeof(fs_req_wrap_t));
+        wrap->buffer = malloc(BUFFER_SIZE);
+        wrap->ctx = ctx;
+
+        uv_buf_t iov = uv_buf_init(wrap->buffer, BUFFER_SIZE);
+        wrap->req.data = wrap;
+
+        uv_fs_read(ctx->loop, &(wrap->req), ctx->file, &iov, 1, ctx->offset, on_read);
+    }
+    else
+    {
+        fprintf(stderr, "error opening file: %s\n", uv_strerror((int)req->result));
+        // 작업이 중간에 실패했으므로, 파일 작업 컨텍스트 객체 또한 회수해야 한다.
+        free(ctx);
     }
 
-    file_context_t* ctx = (file_context_t*)req->data;
-    ctx->fd = (int)req->result;
+    // uv_fs_req_cleanup 함수가 인자로서 받아들이고 있는 req는
+    // main 함수에서 만들어낸 open_req 포인터가 맞다.
+    // 그러나, uv_fs_req_cleanup 함수는 open_req(== on_open() 함수의 req 인자) 자체를
+    // 회수하는 것은 아니다.
+    // 파일 리딩과 관련된 모든 콜백 함수들(on_open, on_close, on_read) 의 끝부분에서
+    // 현재의 함수 호출에서의 작업을 끝낸 후, 항상 uv_fs_req_cleanup()을 호출하고 있는 것에 주목해야 한다.
+    uv_fs_req_cleanup(req);
+}
 
-    // allocate buffer
-    printf("!!! buffer allocated !!!\n");
-    ctx->buffer = uv_buf_init( (char*)malloc(BUFFER_SIZE + 1), BUFFER_SIZE );
+void on_read(uv_fs_t* req)
+{
+    fs_req_wrap_t* wrap = (fs_req_wrap_t*)req->data;
+    file_context_t* ctx = wrap->ctx;
 
-    // start first async read via thread pool
-    // 여기서 malloc 된 work_req는 work_cb와 after_work_cb에 전달되고,
-    // after_wort_cb에서 최종적으로 free 된다.
-    printf("\t!!! work req allocated !!!\n");
-    uv_work_t* work_req = (uv_work_t*)malloc(sizeof(uv_work_t));
-    work_req->data = ctx;
-    uv_queue_work(ctx->loop, work_req, work_cb, after_work_cb);
+    if (req->result < 0)
+    {
+        fprintf(stderr, "Read error: %s\n", uv_strerror((int)req->result));
+        uv_fs_close(ctx->loop, &ctx->close_req, ctx->file, on_close);
+        ctx->close_req.data = ctx;
+    }
+    else if (req->result == 0)
+    {
+        uv_fs_close(ctx->loop, &ctx->close_req, ctx->file, on_close);
+        ctx->close_req.data = ctx;
+    }
+    else
+    {
+        // 파일 리딩 작업을 통해서 읽어들인 내용물을 화면에 표시하기 위한 코드다. stdout 이 들어 있는 것을 보면 알 수 있다.
+        fwrite(wrap->buffer, 1, req->result, stdout);
+
+        // 어디까지 읽었는지를 기록한다.
+        ctx->offset += req->result;
+
+        // 다음 파일 리딩 작업 요청 객체를 할당한 후, 재귀적으로 uv_fs_read()를 호출한다.
+        fs_req_wrap_t* next_wrap = malloc(sizeof(fs_req_wrap_t));
+        next_wrap->buffer = malloc(BUFFER_SIZE);
+        next_wrap->ctx = ctx;
+
+        uv_buf_t iov = uv_buf_init(next_wrap->buffer, BUFFER_SIZE);
+        next_wrap->req.data = next_wrap;
+
+        uv_fs_read(ctx->loop, &next_wrap->req, ctx->file, &iov, 1, ctx->offset, on_read);
+    }
+
+    // 이번 리딩 작업 요청에 사용된 버퍼를 회수한다.
+    free(wrap->buffer);
+    // 현재의 함수 호출의 작업들을 처리한 후, req 요청과 관련하여 libuv 내부에서 사용한 리소스들을 회수한다.
+    uv_fs_req_cleanup(req);
+    // 이번 요청에서 사용한 wrap 객체 즉, 파일 리딩 작업 요청 객체를 완전히 회수한다.
+    free(wrap);
 }
 
 /*
-텍스트 파일을 비동기적으로 열고, 내용물을 한 줄 한 줄 프린트하는 예제이다.
-이 동작은 메인 함수를 실행하는 스레드를 블록킹하지 않는다.
 
-기존의 fread 류의 함수들은 해당 동작을 하는 동안에 main.c 를 실행하는 스레드가 블록킹 된다.
+05-async-file-read
+브랜치에서는 uv_fs_read() 와 같이 libuv에서 지원하는 파일 리딩 api를 사용하지 않고, POSIX 파일 리딩 api를 직접 사용했다.
 
-실행 로그를 보면,
-void on_open(uv_fs_t* req){...}가 작업을 모두 마치기도 전에,
+반면, 이번 구현에서는 uv_fs_read() 함수를 적극 활용했다.
+libuv에서 파일 리딩 작업은 항상 worker thread pool에서 처리하기 때문에,
+05-async-file-read 브랜치에서 구현한 것과 같이 명시적으로 uv_queue_work() 를 통해서 worker threa pool에게
+직접 태스크들을 전달해줄 필요가 없다.
 
-    printf("!!! non blocking work 1 !!!\n");
-    printf("!!! non blocking work 2 !!!\n");
-
-가 먼저 출력되는 것을 확인할 수 있다.
-
-work_cb 함수는 워커 스레드 풀 내에서 실행되고,
-work_cb의 실행이 끝나면 해당 work_cb와 함께 enqueue 됐던 after_work_cb가
-main event loop에서 실행된다.
-
-정리하면,
-work_cb → runs in the worker thread pool (blocking work allowed)
-after_work_cb → runs in the main event loop thread (non-blocking only)
-
-이것이 가능한 이유는, libuv가 기본적으로 두 가지 종류의 executor가 존재하기 때문이다.
-1. worker thread pool
-2. main event loop
-
+둘 중 누가 더 성능이 좋은지는 실제 테스트를 해봐야 알 수 있을 듯 하다.
  */
 int main() {
-    uv_loop_t* loop = uv_default_loop();
-
-    printf("!!! file_context_t allocated !!!\n");
+    // 파일 작업 컨텍스트 객체를 할당한다.
+    // 해당 객체는 파일 작업 컨텍스트 객체는 파일 리딩 작업이 완료(성공이든, 에러든) 될 때까지 살아 있는다.
+    // 즉, 모든 리딩 요청들이 공유하는 객체다.
     file_context_t* ctx = malloc(sizeof(file_context_t));
-    ctx->loop = loop;
-    ctx->open_req.data = ctx;
+    ctx->loop = uv_default_loop();
+    ctx->offset = 0;
 
-    // 파일을 연다.
-    uv_fs_open(loop, &(ctx->open_req), FILE_PATH, O_RDONLY, 0, on_open);
-    printf("!!! non blocking work 1 !!!\n");
-    printf("!!! non blocking work 2 !!!\n");
+    // open req를 할당한다.
+    uv_fs_t* open_req = malloc(sizeof(uv_fs_t));
+    open_req->data = ctx;
 
-    uv_run(loop, UV_RUN_DEFAULT);
-    uv_loop_close(loop);
+    uv_fs_open(ctx->loop, open_req, FILE_PATH, O_RDONLY, 0, on_open);
+
+    printf("!!! not blocked work 1 !!!\n");
+    printf("!!! not blocked work 2 !!!\n");
+
+    uv_run(ctx->loop, UV_RUN_DEFAULT);
+
+    // 모든 파일 리딩 작업이 끝난 후, open req를 회수한다.
+    free(open_req); // clean up open_req after run ends
     return 0;
 }
